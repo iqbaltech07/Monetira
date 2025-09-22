d70ca599b05fe6653a1bced72c92e67c129fbc0c
iqbaltech07 <dev.iqbal007@gmail.com>
2025-09-22 09:07:43 +0700

    feat: setup husky

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+// commitlint.config.js
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // fitur baru
+        "fix", // perbaikan bug
+        "docs", // dokumentasi
+        "style", // formatting, spasi, tanda baca, dll (tanpa mengubah kode)
+        "refactor", // refactor kode tanpa fitur/bugfix
+        "perf", // perbaikan performa
+        "test", // menambahkan/merubah test
+        "build", // build system, dependencies, tools
+        "ci", // continuous integration
+        "chore", // hal kecil lain (misal update config)
+        "revert", // rollback commit sebelumnya
+      ],
+    ],
+  },
+};
